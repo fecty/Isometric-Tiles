9e57da5413de544aafba910222932785fea5dfcc
Zurain Nazir <esfectus@gmail.com>
2025-12-26 22:34:55 +0530

    fix implicit conversion warnings

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,10 @@
 int w = SCREEN_WIDTH;
 int h = SCREEN_HEIGHT;
 Color bgColor = BG_COLOR;
-Color fgColor = {255 - bgColor.r, 255 - bgColor.g, 255 - bgColor.b, 255}; // Opposite of background color
+Color fgColor = {static_cast<unsigned char>(255 - bgColor.r),
+                 static_cast<unsigned char>(255 - bgColor.g),
+                 static_cast<unsigned char>(255 - bgColor.b),
+                 255}; // Opposite of background color
 
 int gridSize = GRID_SIZE; // square grid
 float amplitude = AMPLITUDE;
@@ -23,7 +26,7 @@ Texture tileTex;
 // Function Declarations
 void handleEvents();
 void drawGame();
-void drawTile(int x, int y, Vector2 startPos, int gridSize, float altitude, bool showOutline = false);
+void drawTile(int x, int y, Vector2 startPos, int size, float altitude, bool showOutline = false);
 unsigned int prepareAssets(char *fileLoc);
 Vector2 transform(Vector2 v);
 
@@ -92,7 +95,7 @@ void handleEvents()
 
     amplitude = Clamp(amplitude, 0.f, MAX_AMPLITUDE);
     oscilSpeed = Clamp(oscilSpeed, 0.f, MAX_OSCIL_SPEED);
-    gridSize = static_cast<int>(Clamp(gridSize, 1.f, float(MAX_GRID_SIZE)));
+    gridSize = static_cast<int>(Clamp((float)gridSize, 1.f, float(MAX_GRID_SIZE)));
 
     // cout << "Grid: " << gridSize << "x" << gridSize << "  ";
     // cout << "Oscillation Speed: " << oscilSpeed << "  ";
@@ -104,8 +107,8 @@ void drawGame()
     BeginDrawing();
     ClearBackground(bgColor);
 
-    Vector2 startPos = {(w - tileTex.width) / 2.f, // to center a unit tile to its center
-                        h / 2.f};
+    Vector2 startPos = {((float)w - (float)tileTex.width) / 2.f, // to center a unit tile to its center
+                        (float)h / 2.f};
 
     for (int rowIndex = 0; rowIndex < gridSize; rowIndex++)
     {
@@ -116,14 +119,14 @@ void drawGame()
                 switch (option)
                 {
                 case 1:
-                    return sinf(rowIndex + GetTime() * speed) * maxAlt; // along row
+                    return sinf((float)rowIndex + (float)GetTime() * speed) * maxAlt; // along row
                     break;
                 case 2:
-                    return sinf(colIndex + GetTime() * speed) * maxAlt; // along col
+                    return sinf((float)colIndex + (float)GetTime() * speed) * maxAlt; // along col
                     break;
                 case 3:
                 default:
-                    return sinf(rowIndex + GetTime() * speed) * sinf(colIndex + GetTime() * speed) * maxAlt; // along both
+                    return sinf((float)rowIndex + (float)GetTime() * speed) * sinf((float)colIndex + (float)GetTime() * speed) * maxAlt; // along both
                 }
             };
 
@@ -152,18 +155,18 @@ void drawGame()
     EndDrawing();
 };
 
-void drawTile(int x, int y, Vector2 startPos, int gridSize, float altitude, bool showOutline)
+void drawTile(int x, int y, Vector2 startPos, int size, float altitude, bool showOutline)
 {
 
-    Vector2 isoCoords = transform({x * tileTex.width, y * tileTex.height}); // isometric transformation
-    isoCoords.x = startPos.x + (isoCoords.x / 2.f) - (tileTex.width / 2.f);
-    isoCoords.y = startPos.y + (isoCoords.y / 2.f) - (tileTex.height * gridSize / 4.f);
+    Vector2 isoCoords = transform({float(x * tileTex.width), float(y * tileTex.height)}); // isometric transformation
+    isoCoords.x = startPos.x + (isoCoords.x / 2.f) - (float)(tileTex.width / 2);
+    isoCoords.y = startPos.y + (isoCoords.y / 2.f) - (float)(tileTex.height * size / 4);
 
     isoCoords.y -= altitude; // Makes the tile appear elevated
     if (showOutline)
-        DrawRectangleLines(isoCoords.x, isoCoords.y, tileTex.width, tileImg.height, RED); // Show outline of tiles
+        DrawRectangleLines((int)isoCoords.x, (int)isoCoords.y, tileTex.width, tileImg.height, RED); // Show outline of tiles
 
-    DrawTexture(tileTex, isoCoords.x, isoCoords.y, fgColor);
+    DrawTexture(tileTex, (int)isoCoords.x, (int)isoCoords.y, fgColor);
 }
 
 unsigned int prepareAssets(char *fileLoc)
@@ -188,6 +191,6 @@ Vector2 transform(Vector2 v)
     * v' = Mv
      */
 
-    return {1.0 * v.x - 1.0 * v.y,
-            0.5 * v.x + 0.5 * v.y};
+    return {1.0f * v.x - 1.0f * v.y,
+            0.5f * v.x + 0.5f * v.y};
 }
