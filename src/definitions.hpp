6b3a61b2ba4ff84b150f40fb3508c079cef6547a
Zurain Nazir <esfectus@gmail.com>
2025-12-26 22:47:35 +0530

    add comments to preprocessor defines

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -6,10 +6,10 @@
 #define SCREEN_TITLE "Isometric Tiles Experiment"
 #define BG_COLOR BLACK
 
-#define AMPLITUDE 32
-#define MAX_AMPLITUDE (AMPLITUDE * 5)
-#define OSCIl_SPEED 2 // oscillation speed
-#define MAX_OSCIL_SPEED (OSCIl_SPEED * 3)
-#define GRID_SIZE 15
-#define MAX_GRID_SIZE 50
-#define OSCIL_OPTION 3
+#define AMPLITUDE 32                      // determins the height of each individual tile
+#define MAX_AMPLITUDE (AMPLITUDE * 5)     // max height of an individual tile allowed
+#define OSCIl_SPEED 2                     // oscillation speed
+#define MAX_OSCIL_SPEED (OSCIl_SPEED * 3) // max oscillation speed allowed
+#define GRID_SIZE 15                      // GRID_SIZE * GRID_SIZE is the total number of tiles
+#define MAX_GRID_SIZE 50                  // max grid size allowed
+#define OSCIL_OPTION 3                    // different height functions for an indivdual tile
