60d1ebed34f40c0ddb46c90926d394d73d1cc889
Zurain Nazir <esfectus@gmail.com>
2025-12-26 22:23:41 +0530

    add events and text to program

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -3,5 +3,13 @@
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 800
 #define FPS 60
-#define SCREEN_TITLE "Isometric Tiles"
+#define SCREEN_TITLE "Isometric Tiles Experiment"
 #define BG_COLOR BLACK
+
+#define AMPLITUDE 32
+#define MAX_AMPLITUDE (AMPLITUDE * 5)
+#define OSCIl_SPEED 2 // oscillation speed
+#define MAX_OSCIL_SPEED (OSCIl_SPEED * 3)
+#define GRID_SIZE 15
+#define MAX_GRID_SIZE 50
+#define OSCIL_OPTION 3
--- src/main.cpp
+++ src/main.cpp
@@ -3,106 +3,191 @@
 #include <raylib.h>
 #include <raymath.h>
 
-#include "definitions.hpp"
+#include "definitions.hpp" // Contains constants relevent to program
 
+// Globals
 int w = SCREEN_WIDTH;
 int h = SCREEN_HEIGHT;
 Color bgColor = BG_COLOR;
+Color fgColor = {255 - bgColor.r, 255 - bgColor.g, 255 - bgColor.b, 255}; // Opposite of background color
 
-char imgLoc[] = "assets/grass_tile.png";
+int gridSize = GRID_SIZE; // square grid
+float amplitude = AMPLITUDE;
+float oscilSpeed = OSCIl_SPEED;
+unsigned short oscilOption = OSCIL_OPTION; // for different altitude functions
+
+char imgLoc[] = "assets/grass_tile.png"; // Location of isometric tile
 Image tileImg;
 Texture tileTex;
 
-void HandleEvents();
-void DrawGame();
+// Function Declarations
+void handleEvents();
+void drawGame();
+void drawTile(int x, int y, Vector2 startPos, int gridSize, float altitude, bool showOutline = false);
 unsigned int prepareAssets(char *fileLoc);
-using namespace std;
+Vector2 transform(Vector2 v);
 
+// Entry Point
+using namespace std;
 int main()
 {
     SetTargetFPS(FPS);
     SetTraceLogLevel(LOG_ERROR);
     InitWindow(w, h, SCREEN_TITLE);
 
-    if (prepareAssets(imgLoc))
+    if (prepareAssets(imgLoc)) // Load Image, perform relevent pre-operations and load it as a texture.
         cout << "Loaded texture successfully" << "\n";
     else
+    {
         cout << "Texture loading failed" << "\n";
+        return -1;
+    }
 
     while (!WindowShouldClose())
     {
         if (IsWindowFocused())
-            HandleEvents();
-        DrawGame();
+            handleEvents();
+
+        drawGame();
     };
+
     return 0;
 }
 
-void HandleEvents() {
+void handleEvents()
+{
 
+    // Amplitude
+    if (IsKeyDown(KEY_U))
+        amplitude += .5f;
+    if (IsKeyDown(KEY_J))
+        amplitude -= .5f;
+
+    // Oscillation Speed
+    if (IsKeyDown(KEY_I))
+        oscilSpeed += .1f;
+    if (IsKeyDown(KEY_K))
+        oscilSpeed -= .1f;
+
+    // Grid Size
+    if (IsKeyPressed(KEY_O))
+        gridSize += 1;
+    if (IsKeyPressed(KEY_L))
+        gridSize -= 1;
+
+    if (IsKeyPressed(KEY_ONE))
+        oscilOption = 1;
+    if (IsKeyPressed(KEY_TWO))
+        oscilOption = 2;
+    if (IsKeyPressed(KEY_THREE))
+        oscilOption = 3;
+
+    // Revert to original values
+    if (IsKeyPressed(KEY_SPACE))
+    {
+        gridSize = GRID_SIZE;
+        amplitude = AMPLITUDE;
+        oscilSpeed = OSCIl_SPEED;
+    }
+
+    amplitude = Clamp(amplitude, 0.f, MAX_AMPLITUDE);
+    oscilSpeed = Clamp(oscilSpeed, 0.f, MAX_OSCIL_SPEED);
+    gridSize = static_cast<int>(Clamp(gridSize, 1.f, float(MAX_GRID_SIZE)));
+
+    // cout << "Grid: " << gridSize << "x" << gridSize << "  ";
+    // cout << "Oscillation Speed: " << oscilSpeed << "  ";
+    // cout << "Amplitude: " << amplitude << "\n";
 };
 
-Vector2 transform(Vector2 v)
-{
-    float x = v.x;
-    float y = v.y;
-
-    return {1.0 * x - 1.0 * y,
-            0.5 * x + 0.5 * y};
-}
-
-void drawTile(int x, int y, Vector2 startPos, int gridSize, float altitude)
-{
-
-    Vector2 isoCoords = transform({x * tileTex.width, y * tileTex.height});
-    isoCoords.x = startPos.x + (isoCoords.x / 2.f) - (tileTex.width / 2.f);
-    isoCoords.y = startPos.y + (isoCoords.y / 2.f) - (tileTex.height * gridSize / 4.f);
-
-    isoCoords.y -= altitude;
-    // DrawRectangleLines(isoCoords.x, isoCoords.y, tileTex.width, tileImg.height, RED);
-    DrawTexture(tileTex, isoCoords.x, isoCoords.y, WHITE);
-}
-
-void DrawGame()
+void drawGame()
 {
     BeginDrawing();
     ClearBackground(bgColor);
 
-    int gridSize = 15;
-    Vector2 mousePos = GetMousePosition();
-    Vector2 startPos = {(w - tileTex.width) / 2.f, h / 2.f};
-
-    // DrawTexture(tileTex, startPos.x, startPos.y, WHITE);
+    Vector2 startPos = {(w - tileTex.width) / 2.f, // to center a unit tile to its center
+                        h / 2.f};
 
     for (int rowIndex = 0; rowIndex < gridSize; rowIndex++)
     {
         for (int colIndex = 0; colIndex < gridSize; colIndex++)
         {
-            int i = (gridSize * rowIndex) + colIndex;
-            Vector2 isoCoords = transform({colIndex * tileTex.width, rowIndex * tileTex.height});
-
-            auto getAlt = [&](float speed, float maxAlt)
+            auto getAlt = [&](float speed, float maxAlt, unsigned short option)
             {
-                // return sinf(rowIndex + GetTime() * speed) * maxAlt; // along row
-                // return sinf(colIndex + GetTime() * speed) * maxAlt; // along col
-
-                return sinf(rowIndex + GetTime() * speed) * sinf(colIndex + GetTime() * speed) * maxAlt; // along both
+                switch (option)
+                {
+                case 1:
+                    return sinf(rowIndex + GetTime() * speed) * maxAlt; // along row
+                    break;
+                case 2:
+                    return sinf(colIndex + GetTime() * speed) * maxAlt; // along col
+                    break;
+                case 3:
+                default:
+                    return sinf(rowIndex + GetTime() * speed) * sinf(colIndex + GetTime() * speed) * maxAlt; // along both
+                }
             };
 
-            drawTile(colIndex, rowIndex, startPos, gridSize, getAlt(3, 30));
+            drawTile(colIndex,
+                     rowIndex,
+                     startPos,
+                     gridSize,
+                     getAlt(oscilSpeed, amplitude, oscilOption),
+                     false);
+
+            // cout << "Grid: " << gridSize << "x" << gridSize << "  ";
+            // cout << "Oscillation Speed: " << oscilSpeed << "  ";
+            // cout << "Amplitude: " << amplitude << "\n";
         }
     }
+    DrawText(TextFormat("Grid: %dx%d", gridSize, gridSize), 5, 5, 20, fgColor);
+    DrawText(TextFormat("Oscillation Speed: %.1f", oscilSpeed), 5, 25, 20, fgColor);
+    DrawText(TextFormat("Amplitude: %.1f", amplitude), 5, 45, 20, fgColor);
 
-    DrawCircleV({startPos.x, startPos.y}, 5, RED);
+    DrawText("( I/K ) for Oscillation speed", 5, h - 25, 10, fgColor);
+    DrawText("( U/J ) for Amplitude", 5, h - 15, 10, fgColor);
+    DrawText("( O/L ) for Grid Size", 5, h - 35, 10, fgColor);
+    DrawText("( SPACE ) to Reset", 5, h - 45, 10, fgColor);
+    DrawText("( 1, 2, 3... ) for Options", 5, h - 55, 10, fgColor);
 
     EndDrawing();
 };
 
+void drawTile(int x, int y, Vector2 startPos, int gridSize, float altitude, bool showOutline)
+{
+
+    Vector2 isoCoords = transform({x * tileTex.width, y * tileTex.height}); // isometric transformation
+    isoCoords.x = startPos.x + (isoCoords.x / 2.f) - (tileTex.width / 2.f);
+    isoCoords.y = startPos.y + (isoCoords.y / 2.f) - (tileTex.height * gridSize / 4.f);
+
+    isoCoords.y -= altitude; // Makes the tile appear elevated
+    if (showOutline)
+        DrawRectangleLines(isoCoords.x, isoCoords.y, tileTex.width, tileImg.height, RED); // Show outline of tiles
+
+    DrawTexture(tileTex, isoCoords.x, isoCoords.y, fgColor);
+}
+
 unsigned int prepareAssets(char *fileLoc)
 {
-    tileImg = LoadImage(fileLoc);
-    ImageResizeNN(&tileImg, tileImg.width * 2, tileImg.height * 2);
-    tileTex = LoadTextureFromImage(tileImg);
+    tileImg = LoadImage(fileLoc);                                   // upload to RAM
+    ImageResizeNN(&tileImg, tileImg.width * 2, tileImg.height * 2); // 32x32 -> 64x64 (w/ nearest neighbour)
+    tileTex = LoadTextureFromImage(tileImg);                        // upload to VRAM
+    UnloadImage(tileImg);                                           // unload from RAM
     cout << "Loaded Texture with width: " << tileTex.width << " and height: " << tileTex.height << "\n";
-    return tileTex.id;
+    return tileTex.id; // 0 if texture loading failed
+}
+
+Vector2 transform(Vector2 v)
+{
+    /**
+    * Apply matrix transformation
+    * M = [+1.0 -1.0]
+    *     [+0.5 +0.5]
+
+    * v = <x, y>
+
+    * v' = Mv
+     */
+
+    return {1.0 * v.x - 1.0 * v.y,
+            0.5 * v.x + 0.5 * v.y};
 }
